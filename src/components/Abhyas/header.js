632ebfe4a055d4c453da2420c2d1e5e6188780b9
tri10 <shubhi.blog@gmail.com>
2021-11-16 22:35:43 +0530

    added firebase

--- src/components/Abhyas/header.js
+++ src/components/Abhyas/header.js
@@ -1,14 +1,20 @@
 import Logo from "./logo";
 import Link from 'next/link';
 import React from "react";
+// import { useUser } from "../../../firebase/useUser";
+
 const Header= ()=>{
+    // const {user} = useUser();
+    const isSignedIn = false;
     return (
         <header className="bg-primary w-full m-0 p-2 flex justify-between items-center">
             <Logo />
             <div className="flex justify-around items-center">
-                <Link href='/auth/login' ><a className="text-white hover:text-tertiary m-2" > Login </a></Link>
-                <Link href='/auth/signup'><a  className="text-white hover:text-tertiary m-2" > Signup</a></Link>
-                <div className='w-10 rounded-full h-10 bg-gray-200'  />
+                
+                {!isSignedIn && [<Link key='aaa' href={{pathname:'/auth/login',query:{signup:false}}} ><a className="text-white hover:text-tertiary m-2" > Login </a></Link>,
+                <Link key="bbb" href={{pathname:'/auth/login',query:{signup:true}}}><a  className="text-white hover:text-tertiary m-2" > Signup</a></Link>]}
+                {isSignedIn && <Link href='/auth/logout' ><a className="text-white hover:text-tertiary m-2" > Logout </a></Link> }
+               {isSignedIn &&  <div className='w-10 rounded-full h-10 bg-gray-200 '  ><img  className="rounded-full" src={userProfileUrl} /></div>}
             </div>
 
         </header>
---
+++ src/pages/auth/logout.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import Button from "../../components/UI/button";
+import { useRouter } from "next/router";
+import { useUser } from "../../../firebase/useUser";
+const LogOutPage = ({ isSignOut = true }) => {
+    const router = useRouter();
+    const { logout } = useUser();
+    useEffect(async () => {
+        await logout()
+    }, [])
+
+    const onClickHandler = () => {
+        router.push('/home')
+
+    }
+    return (
+
+        <><h1>Logout {isSignOut ? 'Succesfully' : 'Failed'}</h1>
+            <Button onClick={onClickHandler}>GO HOME</Button></>
+    );
+
+}
+
+export default LogOutPage;
